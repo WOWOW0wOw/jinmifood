5d46d6736fa8c994fc6e31174c69507ce83189bb
WOWOW0wOw <jongseok8529@gmail.com>
2025-09-23 18:50:29 +0900

    dfs
    fds

---
+++ jinmi/src/main/java/com/jinmifood/jinmi/JinmiApplication.java
@@ -0,0 +1,13 @@
+package com.jinmifood.jinmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinmiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JinmiApplication.class, args);
+    }
+
+}
---
+++ jinmi/src/test/java/com/jinmifood/jinmi/JinmiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinmifood.jinmi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JinmiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
