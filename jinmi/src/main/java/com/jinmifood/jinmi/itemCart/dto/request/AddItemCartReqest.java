1e38caa61802d2c28330e07abcc5e2288de12cee
Songmyungbo <audqh656@gmail.com>
2025-09-29 23:59:03 +0900

    0929

--- jinmi/src/main/java/com/jinmifood/jinmi/itemCart/dto/request/AddItemCartReqest.java
+++ jinmi/src/main/java/com/jinmifood/jinmi/itemCart/dto/request/AddItemCartReqest.java
@@ -2,21 +2,21 @@ package com.jinmifood.jinmi.itemCart.dto.request;
 
 import lombok.*;
 
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Getter
-//@Setter
-//@ToString
-//public class AddItemCartReqest {
-//
-//    private int itemId;
-//    private int userId;
-//    private String itemName;
-//    private String itemOption;
-//    private int totalCount;
-//    private int itemPrice;
-//    private int deliveryFee;
-//    private int price;
-//    private int point;
-//    private int totalPrice;
-//}
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class AddItemCartReqest {
+
+    private int itemId;
+    private int userId;
+    private String itemName;
+    private String itemOption;
+    private int totalCount;
+    private int itemPrice;
+    private int deliveryFee;
+    private int price;
+    private int point;
+    private int totalPrice;
+}
